c4d5256c7e1ebf5c6bd57960f3bab096f9401baf
angryparizval <53240846+angryparizval@users.noreply.github.com>
2024-11-06 16:20:36 -0500

    Add project files.

---
+++ CSC 350 start/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+End Class
---
+++ CSC 350 start/RentalAgreeAutoPop.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class RentalAgreeAutoPop
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label8 = New Label()
+        TextBox4 = New TextBox()
+        Label9 = New Label()
+        Label6 = New Label()
+        TextBox3 = New TextBox()
+        Label7 = New Label()
+        Label4 = New Label()
+        TextBox2 = New TextBox()
+        Label5 = New Label()
+        Label3 = New Label()
+        Label2 = New Label()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(477, 254)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(71, 20)
+        Label8.TabIndex = 27
+        Label8.Text = "Store No."
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(553, 254)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(125, 27)
+        TextBox4.TabIndex = 26
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(649, 254)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(0, 20)
+        Label9.TabIndex = 25
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(51, 254)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(140, 20)
+        Label6.TabIndex = 24
+        Label6.Text = "Insurance Policy No."
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(197, 251)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 27)
+        TextBox3.TabIndex = 23
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(293, 251)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(0, 20)
+        Label7.TabIndex = 22
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(464, 156)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(83, 20)
+        Label4.TabIndex = 21
+        Label4.Text = "Vehicle No."
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(553, 153)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 20
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(665, 156)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(0, 20)
+        Label5.TabIndex = 19
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(92, 156)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(99, 20)
+        Label3.TabIndex = 18
+        Label3.Text = "Customer No."
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI Historic", 21F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(197, 29)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(410, 47)
+        Label2.TabIndex = 17
+        Label2.Text = "Rental Agreement Form"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI Historic", 9F, FontStyle.Bold)
+        Button1.Location = New Point(37, 38)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(127, 29)
+        Button1.TabIndex = 16
+        Button1.Text = "Back to Home"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(197, 153)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 15
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(293, 153)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 20)
+        Label1.TabIndex = 14
+        ' 
+        ' RentalAgreeAutoPop
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label8)
+        Controls.Add(TextBox4)
+        Controls.Add(Label9)
+        Controls.Add(Label6)
+        Controls.Add(TextBox3)
+        Controls.Add(Label7)
+        Controls.Add(Label4)
+        Controls.Add(TextBox2)
+        Controls.Add(Label5)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Name = "RentalAgreeAutoPop"
+        Text = "RentalAgreeAutoPop"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
